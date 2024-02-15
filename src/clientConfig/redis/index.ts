87fb097a3ad6617d779e91ad60c88f66fb210f13
Adham Akmal Azmi <adham_92@live.com>
2024-02-15 21:34:43 +0800

    buang line

--- src/clientConfig/redis/index.ts
+++ src/clientConfig/redis/index.ts
@@ -5,15 +5,11 @@
 //   port: 6379, // Redis server port
 //   password: "redis", // Redis server password (if required)
 //   db: 0, // Redis database index
-//   tls: {
-//     // TLS/SSL options (if required)
-//     rejectUnauthorized: false, // Set to `true` if using a self-signed certificate
-//   },
 //   retryStrategy: (times: number) => {
 //     // Retry strategy for failed connection attempts
 //     if (times <= 3) {
 //       return Math.min(times * 100, 3000);
-//     }
+//     }2
 //     return null; // Return `null` to stop retrying
 //   },
 //   connectTimeout: 10000
