0371b780ff5d4327378d0ecb97ff23694ecc774e
Adham Akmal Azmi <adham_92@live.com>
2024-02-15 19:05:46 +0800

    first commit

---
+++ src/clientConfig/redis/index.ts
@@ -0,0 +1,23 @@
+// import { Redis } from "ioredis";
+
+// const redisOptions = {
+//   host: "localhost", // Redis server host
+//   port: 6379, // Redis server port
+//   password: "redis", // Redis server password (if required)
+//   db: 0, // Redis database index
+//   tls: {
+//     // TLS/SSL options (if required)
+//     rejectUnauthorized: false, // Set to `true` if using a self-signed certificate
+//   },
+//   retryStrategy: (times: number) => {
+//     // Retry strategy for failed connection attempts
+//     if (times <= 3) {
+//       return Math.min(times * 100, 3000);
+//     }
+//     return null; // Return `null` to stop retrying
+//   },
+//   connectTimeout: 10000
+// };
+// const redis = new Redis(redisOptions);
+
+// export default { redis };
